b1a95e17ec862da2188a2d0836c896695b2c6ad0
pedro <opedrolps@gmail.com>
2019-10-15 14:34:12 -0300

    estilizar componentes

--- src/app/nav-pokemon/nav-pokemon.component.ts
+++ src/app/nav-pokemon/nav-pokemon.component.ts
@@ -10,18 +10,17 @@ import { Component, OnInit, Input } from '@angular/core';
 export class NavPokemonComponent implements OnInit {
   @Input() pokelist
   @Input() gen
+
   public singlePokemon
   public singlePokemonImg 
+  public isActive: boolean = false
 
   constructor(private router: Router, private route: ActivatedRoute, private service: HttpService) {
 
   }
 
-  /*
-  O retorno está correto
-  */
-
   pokemonDetails(pokemon) {
+    this.isActive = !this.isActive;
 
     this.service.getPokemonData(pokemon).subscribe(dados => {
       this.singlePokemon = dados
@@ -30,6 +29,7 @@ export class NavPokemonComponent implements OnInit {
     this.service.getPokemonMedia(pokemon).subscribe(dados => {
       this.singlePokemonImg = dados
     })
+
     this.router.navigate([`geracao/${this.gen}/`, pokemon.name], { relativeTo: this.route })
   }
 
